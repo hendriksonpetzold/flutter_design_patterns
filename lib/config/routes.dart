a02e06c33de0c8b2b51891378c94077e36ecd5de
Hendrikson Petzold <hendrikson_14@hotmail.com>
2022-11-01 14:18:52 -0300

    feat: routes

---
+++ lib/config/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_design_patterns/pages/home/home_page.dart';
+import 'package:get/get.dart';
+
+class Routes {
+  static final List<GetPage> routes = [
+    GetPage(name: '/', page: () => const HomePage())
+  ];
+}
